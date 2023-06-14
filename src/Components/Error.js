07432a179c2af76d4725e92f6b851bf94176d62d
Stephanie Corrias <scminos@gmail.com>
2023-06-14 16:18:31 +0200

    page error

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import ErrorImg from '../Assets/404.png'
+import '../Styles/Error.css'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
-  return <div></div>
+  return (
+    <div className="error-container">
+      <img src={ErrorImg} alt="erreur" className="error-img" />
+      <div className="error-message">
+        Oups! La page que vous demandez n'existe pas.
+      </div>
+      <Link to="/" className="error-p">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error
